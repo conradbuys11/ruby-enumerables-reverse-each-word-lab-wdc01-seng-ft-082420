9877e2189e8cead501b68dbee3906bc791572914
Conrad Buys <conradbuys11@gmail.com>
2020-08-09 18:20:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(string)
   sentence = string.split(" ")
-  sentence.collect{|word| word.reverse}
+  new_sentence = sentence.collect{|word| word.reverse}
   sentence.join(" ")
 end
