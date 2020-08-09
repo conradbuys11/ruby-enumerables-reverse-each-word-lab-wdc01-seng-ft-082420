e362ddf8b8bfc38d5b5ccf5e703b0dff6bf06174
Conrad Buys <conradbuys11@gmail.com>
2020-08-09 18:14:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
-  sentence_array = %w(string)
-  sentence_array.each do |word|
+  %w(string)
+  string.each do |word|
     word.reverse
   end
-  return sentence_array
+  return string
 end
